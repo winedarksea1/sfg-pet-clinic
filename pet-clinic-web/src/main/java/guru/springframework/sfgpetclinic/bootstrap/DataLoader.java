0dbd12584cd224fd9fce4072bc663e10ce7b79f9
Andrew McGovern <andrew.mcgovern@ruralsource.com>
2019-11-26 15:19:18 -0700

    Added Vet List and updated controller. Closes #15

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -47,7 +47,7 @@ public class DataLoader implements CommandLineRunner {
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
-        vetService.save(vet1);
+        vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
     }
