7f1621a3db3f3c7a578ccf2fa56a2d3b30e14e30
Andrew McGovern <andrew.mcgovern@ruralsource.com>
2019-12-10 11:21:34 -0700

    Added Specialties Map Impl. Closes #31

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Speciality {
+public class Speciality extends BaseEntity {
     private String description;
 
     public String getDescription() {
